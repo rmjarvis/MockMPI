a2d5c63ca00cac9effee2e2b1d8d8f12e2e1bc5d
Mike Jarvis <michael@jarvis.net>
2020-07-30 18:05:44 -0400

    Different API for passing args ans kwargs

--- tests/test_args.py
+++ tests/test_args.py
@@ -7,8 +7,9 @@ def f(comm, txt, index=0, index2=0):
 
 
 def test_args():
-    mock_mpiexec(2, f, args=("abc",))
-    mock_mpiexec(2, f, args=["abc"])
-    mock_mpiexec(
-        2, f, args=["abc"], kwargs={"index": "cat", "index2": "cat"}
-    )
+    mock_mpiexec(2, f, "abc")
+    mock_mpiexec(2, f, "abc", index="cat", index2="cat")
+    args = ["abc"]
+    mock_mpiexec(2, f, *args)
+    kwargs = {"index": "cat", "index2": "cat"}
+    mock_mpiexec(2, f, *args, **kwargs)
