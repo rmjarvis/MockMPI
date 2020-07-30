bf675254b76c7456c89782f96f3a92f842dc8751
Mike Jarvis <michael@jarvis.net>
2020-07-30 18:17:13 -0400

    Also show dereferencing literals

--- tests/test_args.py
+++ tests/test_args.py
@@ -7,9 +7,18 @@ def f(comm, txt, index=0, index2=0):
 
 
 def test_args():
+    # direct args
     mock_mpiexec(2, f, "abc")
     mock_mpiexec(2, f, "abc", index="cat", index2="cat")
+
+    # if have args as list and kwargs as dict, can dereference them.
     args = ["abc"]
     mock_mpiexec(2, f, *args)
     kwargs = {"index": "cat", "index2": "cat"}
     mock_mpiexec(2, f, *args, **kwargs)
+
+    # can also dereference as literals, which is parobably weird, but allowed.
+    mock_mpiexec(2, f, *["abc"], **{"index": "cat", "index2": "cat"})
+
+if __name__ == '__main__':
+    test_args()
