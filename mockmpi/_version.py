7629cbbde0316eb89ec409c5ea2d49d01877e886
Mike Jarvis <michael@jarvis.net>
2022-03-25 15:17:36 -0400

    v0.8.1

--- mockmpi/_version.py
+++ mockmpi/_version.py
@@ -1,6 +1,6 @@
 # Copyright (c) Mike Jarvis and other collaborators
 # See https://github.com/rmjarvis/MockMPI/LICENSE for license information.
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 __version_info__ = tuple(map(int, __version__.split('.')))
 
